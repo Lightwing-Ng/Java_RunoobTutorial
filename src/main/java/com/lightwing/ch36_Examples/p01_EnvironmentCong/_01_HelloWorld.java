ec90976484ceb04c6dcbcdfebd422501c53b6446
Lightwing Ng <rodney_ng@icloud.com>
2018-08-10 14:26:57 +0800

    environment configuration

---
+++ src/main/java/com/lightwing/ch36_Examples/p01_EnvironmentCong/_01_HelloWorld.java
@@ -0,0 +1,23 @@
+package com.lightwing.ch36_Examples.p01_EnvironmentCong;
+
+/**
+ * @ClassName HelloWorld
+ * @Description TODO
+ * @Author Lightwing Ng
+ * @DateTime 2018/8/10, 13:35
+ * @Version 1.0
+ **/
+public class _01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+// $ javac _01_HelloWorld.java
+
+// $ java _01_HelloWorld
+
+// java -classpath com.lightwing.ch36_Examples.p01_EnvironmentCong _01_HelloWorld
+
+// java -version
+
